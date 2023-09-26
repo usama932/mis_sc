4d5cf5cac3f07f7474c01b2eba85030be6a45e33
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-09-26 14:55:16 +0500

    Update _user-account-menu.blade.php

--- resources/views/partials/menus/_user-account-menu.blade.php
+++ resources/views/partials/menus/_user-account-menu.blade.php
@@ -30,7 +30,7 @@
     <!--end::Menu separator-->
     <!--begin::Menu item-->
     <div class="menu-item px-5">
-        <a href="#" class="menu-link px-5">My Profile</a>
+        <a href="" class="menu-link px-5">Update Password</a>
     </div>
 
 
