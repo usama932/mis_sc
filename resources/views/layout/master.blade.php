b81899ac25fa9b4144ca8436707b362e3b33dd14
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-09-28 12:19:50 +0500

    push

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -43,7 +43,7 @@
 <!--end::Head-->
 
 <!--begin::Body-->
-<body {!! printHtmlClasses('body') !!} {!! printHtmlAttributes('body') !!}>
+<body class="app-default" id="kt_app_body" data-kt-name="metronic" data-kt-app-layout="light-sidebar" data-kt-app-header-fixed="true" data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-hoverable="true" data-kt-app-sidebar-push-header="true" data-kt-app-sidebar-push-toolbar="true" data-kt-app-sidebar-push-footer="true" data-kt-app-toolbar-enabled="true">
 
 @include('partials/theme-mode/_init')
 
