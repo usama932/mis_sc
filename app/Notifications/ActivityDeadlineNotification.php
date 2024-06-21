894f31fc777ef7685615600b27ec043a27b14fdc
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2024-06-21 14:46:33 +0500

    push

--- app/Notifications/ActivityDeadlineNotification.php
+++ app/Notifications/ActivityDeadlineNotification.php
@@ -11,40 +11,40 @@ use Illuminate\Notifications\Notification;
 class ActivityDeadlineNotification extends Notification
 {
     use Queueable;
+
     protected $month;
 
     public function __construct($month)
     {
         $this->month = $month;
     }
- 
-    public function via(object $notifiable): array
+
+    public function via($notifiable)
     {
-        return ['mail', 'broadcast'];
+        return [ 'broadcast'];
     }
 
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The deadline for the following activity is the day after tomorrow:')
-                    ->line($this->month->name)
-                    ->action('View Project Activity', url('/activity_dips/'.$this->month?->activity->id))
-                    ->line('Thank you for using our application!');
+            ->line('The deadline for the following activity is the day after tomorrow:')
+            ->line($this->month->name)
+            ->action('View Project Activity', url('/activity_dips/'.$this->month->activity->id))
+            ->line('Thank you for using our application!');
     }
 
     public function toBroadcast($notifiable)
     {
         return new BroadcastMessage([
-            'message' => 'The deadline for the activity "'.$this->month?->activity->name.'" is the day after tomorrow.'
+            'message' => 'The deadline for the activity "'.$this->month->activity->name.'" is the day after tomorrow.'
         ]);
     }
 
     public function toArray($notifiable)
     {
         return [
-            'activity_id' => $this->month->id,
-            'message' => 'The deadline for the activity "'.$this->month?->activity->name.'" is the day after tomorrow.'
+            'activity_id' => $this->month->activity->id,
+            'message' => 'The deadline for the activity "'.$this->month->activity->name.'" is the day after tomorrow.'
         ];
     }
-
 }
--- resources/views/pusher.blade.php
+++ resources/views/pusher.blade.php
@@ -1,59 +1,28 @@
-{{-- 
-<!DOCTYPE html>
-  <head>
-    <title>Pusher Test</title>
-    <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
-    <script>
-  
-      // Enable pusher logging - don't include this in production
-      Pusher.logToConsole = true;
-  
-      var pusher = new Pusher('03ea741e62a6857215b7', {
-        cluster: 'ap2'
-      });
-  
-      var channel = pusher.subscribe('my-channel');
-      channel.bind('my-event', function(data) {
-        alert(JSON.stringify(data));
-      });
-    </script>
-  </head>
-  <body>
-    <h1>Pusher Test</h1>
-    <p>
-      Try publishing an event to channel <code>my-channel</code>
-      with event name <code>my-event</code>.
-    </p>
-  </body>
 
-</html> --}}
 <!DOCTYPE html>
-<html>
 <head>
-    <title>Notifications</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/laravel-echo/1.11.2/echo.iife.js"></script>
+  <title>Pusher Test</title>
+  <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
+  <script>
+
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher('03ea741e62a6857215b7', {
+      cluster: 'ap2'
+    });
+
+    var channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', function(data) {
+      alert(JSON.stringify(data));
+    });
+  </script>
 </head>
 <body>
-    <!-- Your Blade content -->
-
-    <script type="text/javascript">
-        Pusher.logToConsole = true;
-
-        window.Echo = new Echo({
-            broadcaster: 'pusher',
-            key: '{{ env('PUSHER_APP_KEY') }}',
-            cluster: '{{ env('PUSHER_APP_CLUSTER') }}',
-            forceTLS: true
-        });
-
-        window.Echo.private('App.User.{{ Auth::id() }}')
-            .notification((notification) => {
-                console.log(notification.message);
-                alert(notification.message);
-                // You can also dynamically update the DOM to show the notification
-            });
-    </script>
+  <h1>Pusher Test</h1>
+  <p>
+    Try publishing an event to channel <code>my-channel</code>
+    with event name <code>my-event</code>.
+  </p>
 </body>
 </html>
