8c4a3e3e0a0592aa13933d8eb2f5aed3411671ef
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2024-07-03 09:38:07 +0500

    push

--- app/Http/Controllers/Admin/DipActivityController.php
+++ app/Http/Controllers/Admin/DipActivityController.php
@@ -262,64 +262,74 @@ class DipActivityController extends Controller
     
         if ($sortedActivities) {
             foreach ($sortedActivities as $r) {
-                $show_url = route('activity_dips.show', $r->id);
-                $edit_url = route('activity_dips.edit', $r->id);
-                $progress_url = route('postprogress', $r->id);
-                $text = $r->activity_title ?? "";
-                $words = str_word_count($text, 1);
-                $lines = array_chunk($words, 10);
-                $finalText = implode("<br>", array_map(function ($line) {
-                    return implode(" ", $line);
-                }, $lines));
-    
-                $nestedData['activity_number'] = $finalText ?? '';
-                $nestedData['activity'] = $r->activity_number ?? '';
-                $nestedData['theme'] = $r->scisubtheme_name?->maintheme?->name ?? '';
-                $nestedData['sub_theme'] =  $r->scisubtheme_name?->name ?? '';
-                $nestedData['activity_type'] = $r->activity_type?->activity_type?->name 
-                             ? ($r->activity_type?->activity_type?->name . ' (' . $r->activity_type?->name . ')') 
-                             : '';
-                $nestedData['project'] = $r->project->name ?? '';
-                $nestedData['lop_target'] = $r->lop_target ?? '';
-                $quarterTarget = '<ul style="list-style-type: none; padding: 0; margin: 0;">';
-                foreach ($r->months as $month) {
-                    if ($month->activity_id == $r->id && $month->project_id == $r->project_id) {
-                        $quarterTarget .= '<li><strong>' . $month->quarter.'-'.$month->year . ':</strong> ' . $month->target . '</li>';
+
+                $activityId = $r->id;
+                $activitiesWithAllProgress = DipActivity::where('id', $activityId)
+                                                ->whereDoesntHave('months', function ($query) {
+                                                    $query->whereDoesntHave('progress');
+                                                })->pluck('id')->toArray();
+                
+                if (in_array($r->id, $activitiesWithAllProgress)) {
+                    $show_url = route('activity_dips.show', $r->id);
+                    $edit_url = route('activity_dips.edit', $r->id);
+                    $progress_url = route('postprogress', $r->id);
+                    $text = $r->activity_title ?? "";
+                    $words = str_word_count($text, 1);
+                    $lines = array_chunk($words, 10);
+                    $finalText = implode("<br>", array_map(function ($line) {
+                        return implode(" ", $line);
+                    }, $lines));
+        
+                    $nestedData['activity_number'] = $finalText ?? '';
+                    $nestedData['activity'] = $r->activity_number ?? '';
+                    $nestedData['theme'] = $r->scisubtheme_name?->maintheme?->name ?? '';
+                    $nestedData['sub_theme'] =  $r->scisubtheme_name?->name ?? '';
+                    $nestedData['activity_type'] = $r->activity_type?->activity_type?->name 
+                                 ? ($r->activity_type?->activity_type?->name . ' (' . $r->activity_type?->name . ')') 
+                                 : '';
+                    $nestedData['project'] = $r->project->name ?? '';
+                    $nestedData['lop_target'] = $r->lop_target ?? '';
+                    $quarterTarget = '<ul style="list-style-type: none; padding: 0; margin: 0;">';
+                    foreach ($r->months as $month) {
+                        if ($month->activity_id == $r->id && $month->project_id == $r->project_id) {
+                            $quarterTarget .= '<li><strong>' . $month->quarter.'-'.$month->year . ':</strong> ' . $month->target . '</li>';
+                        }
                     }
-                }
-                $quarterTarget .= '</ul>';
-                $nestedData['quarter_target'] = $quarterTarget;
-                $nestedData['created_by'] = $r->user->name ?? '';
-                $nestedData['created_at'] = date('M d, Y', strtotime($r->created_at)). '<br>'. date('h:iA', strtotime($r->created_at)) ?? '';
-                $activity = DipActivity::find($r->id);
-                
-                $nestedData['update_progress'] = '<a href="' . $progress_url . '"><span class="badge badge-success">Update Progress</span></a>';
-              
-                
-                $nestedData['action'] = '<div>
-                                        <td>
-                                            <a class="btn-icon mx-1" href="' . $show_url . '" title="Show Activity" href="javascript:void(0)">
-                                                <i class="fa fa-eye text-warning" aria-hidden="true"></i>
-                                            </a>';
-                                            if (!empty($request->url) && $request->url == 'quarter_progress') {
-                                                $nestedData['action'] .= ' ';
-                                            } else {
-                                                $nestedData['action'] .= '
-                                                <a class="btn-icon mx-1" href="' . $edit_url . '" title="Edit Activity" href="javascript:void(0)">
-                                                    <i class="fa fa-pencil text-info" aria-hidden="true"></i>
+                    $quarterTarget .= '</ul>';
+                    $nestedData['quarter_target'] = $quarterTarget;
+                    $nestedData['created_by'] = $r->user->name ?? '';
+                    $nestedData['created_at'] = date('M d, Y', strtotime($r->created_at)). '<br>'. date('h:iA', strtotime($r->created_at)) ?? '';
+                    $activity = DipActivity::find($r->id);
+                    
+                    $nestedData['update_progress'] = '<a href="' . $progress_url . '"><span class="badge badge-success">Update Progress</span></a>';
+                  
+                    
+                    $nestedData['action'] = '<div>
+                                            <td>
+                                                <a class="btn-icon mx-1" href="' . $show_url . '" title="Show Activity" href="javascript:void(0)">
+                                                    <i class="fa fa-eye text-warning" aria-hidden="true"></i>
                                                 </a>';
-                                            }
-    
-                                            if (auth()->user()->user_type == 'admin') {
-                                                $nestedData['action'] .= '
-                                                                        <a class="btn-icon mx-1" onclick="event.preventDefault();del(' . $r->id . ');" title="Delete Activity" href="javascript:void(0)">
-                                                                            <i class="fa fa-trash text-danger" aria-hidden="true"></i>
-                                                                        </a>';
-                                            }
-    
-                $nestedData['action'] .= '</td></div>';
-    
-                $data[] = $nestedData;
+                                                if (!empty($request->url) && $request->url == 'quarter_progress') {
+                                                    $nestedData['action'] .= ' ';
+                                                } else {
+                                                    $nestedData['action'] .= '
+                                                    <a class="btn-icon mx-1" href="' . $edit_url . '" title="Edit Activity" href="javascript:void(0)">
+                                                        <i class="fa fa-pencil text-info" aria-hidden="true"></i>
+                                                    </a>';
+                                                }
+        
+                                                if (auth()->user()->user_type == 'admin') {
+                                                    $nestedData['action'] .= '
+                                                                            <a class="btn-icon mx-1" onclick="event.preventDefault();del(' . $r->id . ');" title="Delete Activity" href="javascript:void(0)">
+                                                                                <i class="fa fa-trash text-danger" aria-hidden="true"></i>
+                                                                            </a>';
+                                                }
+        
+                    $nestedData['action'] .= '</td></div>';
+        
+                    $data[] = $nestedData;
+                }
+              
             }
         }
     
