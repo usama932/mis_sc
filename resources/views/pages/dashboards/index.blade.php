9f085354274538d3a19ca018cec9380429e4d2ff
Usama Qayyum <usama1517a@gmail.com>
2024-09-25 16:22:33 +0500

    Update index.blade.php

--- resources/views/pages/dashboards/index.blade.php
+++ resources/views/pages/dashboards/index.blade.php
@@ -192,86 +192,98 @@
                     <h2 class="header-title">Project DIP Analytics</h2>
                 </div>
                 <div class="card-body">
-                    <!-- Project Selection -->
-                    <div class="row mb-3">
-                        <div class="col-md-4">
-                            <select name="project" id="project_id" class="form-select btn-select fs-6" aria-label="Select a Project">
-                                <option value="" class="fs-8">Select a Project</option>
-                                @foreach ($projects as $project)
-                                    <option value="{{ $project->id }}" class="fs-8">{{ $project->name }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                 
-                    <div class="row g-4 my-4" id="theme-cards-container"></div>
-                    <!-- Charts Section -->
-                    <div class="row g-4">
-                        <!-- Google Chart -->
-                        <div class="col-md-6">
-                            <div class="chart-container border-danger">
-                                <div class="chart-title text-danger">Organization Chart of Pakistan</div>
-                                <div id="chart_div"></div>
-                                <div class="district-card mt-3">
-                                    <h5 class="text-danger"><i>Implemented By:</i></h5>
-                                    <div class="partner-info"  id="partner_info">
-                                        
+                    <div class="tab-content" id="myTabContent">
+                        <!-- Tab 1: Chart -->
+                        <div class="tab-pane fade show active" id="chart" role="tabpanel" aria-labelledby="chart-tab">
+                            <div class="row mb-3">
+                                <div class="col-md-4">
+                                    <select name="project" id="project_id" class="form-select btn-select fs-6" aria-label="Select a Project">
+                                        <option value="" class="fs-8">Select a Project</option>
+                                        @foreach ($projects as $project)
+                                            <option value="{{ $project->id }}" class="fs-8">{{ $project->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row g-4 my-4" id="theme-cards-container"></div>
+                      
+                            <!-- Charts Section -->
+                            <div class="row g-4">
+                                <!-- Google Chart -->
+                                <div class="col-md-6">
+                                    <div class="chart-container border-danger">
+                                        <div class="chart-title text-danger">Organization Chart of Pakistan</div>
+                                        <div id="chart_div"></div>
+                                        <div class="district-card mt-3">
+                                            <h5 class="text-danger"><i>Implemented By:</i></h5>
+                                            <div class="partner-info"  id="partner_info">
+                                                
+                                            </div>
+                                        </div>
+                            
                                     </div>
                                 </div>
-                    
+                                <!-- Sunburst Chart -->
+                                <div class="col-md-6 ">
+                                    <div class="chart-container border-danger">
+                                        <div class="chart-title text-danger">Theme-wise Progress Analysis</div>
+                                        <div id="main" style="width: 100%; height: 100%;"></div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <!-- Sunburst Chart -->
-                        <div class="col-md-6 ">
-                            <div class="chart-container border-danger">
-                                <div class="chart-title text-danger">Theme-wise Progress Analysis</div>
-                                <div id="main" style="width: 100%; height: 100%;"></div>
+                        <div class="tab-pane " id="table" role="tabpanel" aria-labelledby="table-tab">
+                            <div class="container m-3">
+                                <div class="table-responsive overflow-*">
+                                    <table class="table table-striped table-bordered nowrap" style="width:100%">
+                                        <thead>
+                                            <tr>
+                                                <th class="fs-9">Project</th>
+                                                <th class="fs-9">Type</th>
+                                                <th class="fs-9">SOF</th>
+                                                <th class="fs-9">SCI OPs FP</th>
+                                                <th class="fs-9">Budget Holder</th>
+                                                <th class="fs-9">Tenure</th>
+                                                <th class="fs-9">DIP</th>
+                                                <th class="fs-9">Total Activities</th>
+                                                <th class="fs-9">Total Targets</th>
+                                                <th class="fs-9">Complete Targets</th>
+                                                <th class="fs-9">Overdue Targets</th>
+                                                <th class="fs-9">Pending Targets</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($project_data as $project)
+                                            <tr>
+                                                <td class="fs-9">{{ $project->name }}</td>
+                                                <td class="fs-9">{{ $project->type }}</td>
+                                                <td class="fs-9">{{ $project->sof }}</td>
+                                                <td class="fs-9">{{ $project->focal_person }}</td>
+                                                <td class="fs-9">{{ $project->budget_holder }}</td>
+                                                <td class="fs-9">{{ date('M d,Y', strtotime($project->start_date))}} - {{date('M d,Y', strtotime($project->end_date));}}</td>
+                                                <td class="fs-9">@if($project->activities->count() > 0) Yes @else No @endif</td>
+                                                <td class="fs-9">{{ $project->total_activities_count }}</td>
+                                                <td class="fs-9">{{ $project->total_activities_target_count }}</td>
+                                                <td class="fs-9">{{ $project->complete_activities_count }}</td>
+                                                <td class="fs-9">{{ $project->overdue_count }}</td>
+                                                <td class="fs-9">{{ $project->pending_count }}</td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-
-                    <div class="container m-3">
-                        <div class="table-responsive overflow-*">
-                            <table class="table table-striped table-bordered nowrap" style="width:100%">
-                                <thead>
-                                    <tr>
-                                        <th class="fs-9">Project</th>
-                                        <th class="fs-9">Type</th>
-                                        <th class="fs-9">SOF</th>
-                                        <th class="fs-9">SCI OPs FP</th>
-                                        <th class="fs-9">Budget Holder</th>
-                                        <th class="fs-9">Tenure</th>
-                                        <th class="fs-9">DIP</th>
-                                        <th class="fs-9">Total Activities</th>
-                                        <th class="fs-9">Total Targets</th>
-                                        <th class="fs-9">Complete Targets</th>
-                                        <th class="fs-9">Overdue Targets</th>
-                                        <th class="fs-9">Pending Targets</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($project_data as $project)
-                                    <tr>
-                                        <td class="fs-9">{{ $project->name }}</td>
-                                        <td class="fs-9">{{ $project->type }}</td>
-                                        <td class="fs-9">{{ $project->sof }}</td>
-                                        <td class="fs-9">{{ $project->focal_person }}</td>
-                                        <td class="fs-9">{{ $project->budget_holder }}</td>
-                                        <td class="fs-9">{{ date('M d,Y', strtotime($project->start_date))}} - {{date('M d,Y', strtotime($project->end_date));}}</td>
-                                        <td class="fs-9">@if($project->activities->count() > 0) Yes @else No @endif</td>
-                                        <td class="fs-9">{{ $project->total_activities_count }}</td>
-                                        <td class="fs-9">{{ $project->total_activities_target_count }}</td>
-                                        <td class="fs-9">{{ $project->complete_activities_count }}</td>
-                                        <td class="fs-9">{{ $project->overdue_count }}</td>
-                                        <td class="fs-9">{{ $project->pending_count }}</td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
                         </div>
                     </div>
                 </div>
-
+                <ul class="nav nav-tabs justify-content-start mt-4 mx-9" id="myTab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active" id="chart-tab" data-bs-toggle="tab" href="#chart" role="tab" aria-controls="chart" aria-selected="true"><h6>Project Analytics</h6></a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link" id="table-tab" data-bs-toggle="tab" href="#table" role="tab" aria-controls="table" aria-selected="false"><h6>Project Table</h6></a>
+                    </li>
+                </ul>
                 <div class="card-footer bg-danger text-white p-0 m-0">
                     <h6 class="header-title  p-0 m-0">Project Response Analytics</h6>
                 </div>
